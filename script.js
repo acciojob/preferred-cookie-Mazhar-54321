d10368310142ec5dcfaf5d8f09eaaa2fb9ca2b8a
Mazhar-54321 <160312563+Mazhar-54321@users.noreply.github.com>
2025-05-13 05:50:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("form");
+  const fontsize = document.getElementById("fontsize");
+  console.log(getCookie("fontsize"))
+	fontsize.value=getCookie("fontsize")??'16'
+    const fontcolor = document.getElementById("fontcolor");
+	fontcolor.value=getCookie("fontcolor")??"black"
+  form.addEventListener("submit", (event) => {
+    event.preventDefault(); // stop form from reloading the page
+
+    const fontsize = document.getElementById("fontsize").value;
+    const fontcolor = document.getElementById("fontcolor").value;
+
+    // Store in cookies
+    document.cookie = `fontsize=${fontsize}`;
+    document.cookie = `fontcolor=${fontcolor}`;
+
+    alert("Data saved in cookies!");
+  });
+});
+function getCookie(key) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${key}=`);
+  if (parts.length === 2) return parts.pop().split(";").shift();
+}
